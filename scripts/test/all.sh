8ab091f4a076a310140a426d9e3dd7be94d80365
Gabriel Handford <gabrielh@gmail.com>
2019-12-09 11:00:08 -0800

    scripts: Test all

---
+++ scripts/test/all.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -e -u -o pipefail # Fail on error
+
+dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd $dir
+
+./key.sh
+./encrypt.sh
+./sign.sh
--- scripts/test/key.sh
+++ scripts/test/key.sh
@@ -5,21 +5,21 @@ set -e -u -o pipefail # Fail on error
 echo "gen"
 kid=`keys generate -publish`
 echo "gen $kid"
+echo "push"
+keys push -kid "$kid"
+echo "push"
+keys push "$kid"
 echo "backup"
 seed=`keys backup -kid $kid`
 echo "backup $seed"
 echo "remove"
 keys remove -seed-phrase "$seed" -kid "$kid"
+echo "recover"
+keys recover -seed-phrase "$seed"
+seed=`keys backup $kid`
+echo "backup $seed"
 echo "pull"
 keys pull -kid "$kid"
-
-
-echo "gen"
-kid=`keys generate`
-echo "gen $kid"
-echo "push"
-keys push -kid "$kid"
-echo "remove"
-keys remove -seed-phrase "`keys backup -kid $kid`" -kid "$kid"
 echo "pull"
-keys pull -kid "$kid"
+keys pull "$kid"
+
