c54ea8a045ddcda4b792c26f9fd0cabb60adfbb3
Gabriel Handford <gabrielh@gmail.com>
2020-01-07 17:08:58 -0800

    service: Messages

---
+++ service/messages.go
@@ -0,0 +1,255 @@
+package service
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"sort"
+	"time"
+
+	"github.com/keys-pub/keys"
+	"github.com/keys-pub/keys/saltpack"
+	"github.com/pkg/errors"
+)
+
+// MessagePrepare (RPC) prepares to create a message, the response can be used to show a pending message
+func (s *service) MessagePrepare(ctx context.Context, req *MessagePrepareRequest) (*MessagePrepareResponse, error) {
+	message, prepareErr := s.messagePrepare(ctx, req.Sender, req.KID, req.Text)
+	if prepareErr != nil {
+		return nil, prepareErr
+	}
+	return &MessagePrepareResponse{
+		Message: message,
+	}, nil
+}
+
+// MessageCreate (RPC) creates a message for a recipient
+func (s *service) MessageCreate(ctx context.Context, req *MessageCreateRequest) (*MessageCreateResponse, error) {
+	message, createErr := s.messageCreate(ctx, req.Sender, req.KID, req.ID, req.Text)
+	if createErr != nil {
+		return nil, createErr
+	}
+	return &MessageCreateResponse{
+		Message: message,
+	}, nil
+}
+
+// Messages (RPC) lists messages.
+func (s *service) Messages(ctx context.Context, req *MessagesRequest) (*MessagesResponse, error) {
+	if req.KID == "" {
+		return nil, errors.Errorf("no kid specified")
+	}
+	key, err := s.parseKey(req.KID)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := s.pullMessages(ctx, key.ID()); err != nil {
+		return nil, err
+	}
+
+	messages, messagesErr := s.messages(ctx, key)
+	if messagesErr != nil {
+		return nil, messagesErr
+	}
+	return &MessagesResponse{
+		Messages: messages,
+	}, nil
+}
+
+// Inbox (RPC)
+func (s *service) Inbox(ctx context.Context, req *InboxRequest) (*InboxResponse, error) {
+	return nil, errors.Errorf("not implemented")
+}
+
+// messagePrepare returns a Message for an in progress display. The client
+// should then use messageCreate to save the message. This needs to be fast, so
+// the client can show the a pending message right away. Preparing before create
+// is optional.
+func (s *service) messagePrepare(ctx context.Context, sender string, kid string, text string) (*Message, error) {
+	if kid == "" {
+		return nil, errors.Errorf("no kid specified")
+	}
+	key, err := s.parseKeyOrCurrent(sender)
+	if err != nil {
+		return nil, err
+	}
+	_, err = s.parseKey(kid)
+	if err != nil {
+		return nil, err
+	}
+
+	message := &Message{
+		ID: keys.RandString(32),
+		Content: &MessageContent{
+			Text: text,
+		},
+	}
+
+	s.fillMessage(ctx, message, time.Now(), key.ID(), "")
+	return message, nil
+}
+
+func (s *service) messageCreate(ctx context.Context, sender string, kid string, id string, text string) (*Message, error) {
+	if kid == "" {
+		return nil, errors.Errorf("no kid specified")
+	}
+	if id == "" {
+		id = keys.RandString(32)
+	}
+
+	senderKey, err := s.parseKeyOrCurrent(sender)
+	if err != nil {
+		return nil, err
+	}
+
+	key, err := s.parseKey(kid)
+	if err != nil {
+		return nil, err
+	}
+
+	message := &Message{
+		ID: id,
+		Content: &MessageContent{
+			Text: text,
+		},
+	}
+	b, err := json.Marshal(message)
+	if err != nil {
+		return nil, err
+	}
+
+	sp := saltpack.NewSaltpack(s.ks)
+	encrypted, err := sp.Signcrypt(b, senderKey, key.ID())
+	if err != nil {
+		return nil, err
+	}
+
+	if s.remote == nil {
+		return nil, errors.Errorf("no remote set")
+	}
+	if err := s.remote.PutMessage(key, id, encrypted); err != nil {
+		return nil, err
+	}
+
+	// TODO: Sync to local
+
+	return message, nil
+}
+
+func (s *service) fillMessage(ctx context.Context, message *Message, t time.Time, sender keys.ID, path string) {
+	user, resolveErr := s.findUser(ctx, sender)
+	if resolveErr != nil {
+		logger.Errorf("Failed to resolve user: %s", resolveErr)
+	}
+	message.User = userToRPC(user)
+	message.Sender = sender.String()
+	message.CreatedAt = int64(keys.TimeToMillis(t))
+	message.TimeDisplay = timeDisplay(t)
+	message.DateDisplay = dateDisplay(t)
+	message.Path = path
+}
+
+func (s *service) message(ctx context.Context, path string) (*Message, error) {
+	doc, err := s.db.Get(ctx, path)
+	if err != nil {
+		return nil, err
+	}
+	if doc == nil {
+		return nil, nil
+	}
+
+	sp := saltpack.NewSaltpack(s.ks)
+	decrypted, sender, err := sp.SigncryptOpen(doc.Data)
+	if err != nil {
+		return nil, err
+	}
+
+	var message Message
+	if err := json.Unmarshal(decrypted, &message); err != nil {
+		return nil, err
+	}
+	createdAt := doc.CreatedAt
+	s.fillMessage(ctx, &message, createdAt, sender, path)
+	return &message, nil
+}
+
+func (s *service) messages(ctx context.Context, key *keys.SignKey) ([]*Message, error) {
+	path := fmt.Sprintf("messages-%s", key.ID())
+	iter, iterErr := s.db.Documents(ctx, path, &keys.DocumentsOpts{PathOnly: true})
+	if iterErr != nil {
+		return nil, iterErr
+	}
+	defer iter.Release()
+	messages := make([]*Message, 0, 100)
+	for {
+		e, err := iter.Next()
+		if err != nil {
+			return nil, err
+		}
+		if e == nil {
+			break
+		}
+		logger.Debugf("Message %s", e.Path)
+		message, messageErr := s.message(ctx, e.Path)
+		if messageErr != nil {
+			return nil, messageErr
+		}
+		messages = append(messages, message)
+	}
+
+	sort.Slice(messages, func(i, j int) bool {
+		return messages[i].CreatedAt < messages[j].CreatedAt
+	})
+
+	return messages, nil
+}
+
+func (s *service) pullMessages(ctx context.Context, kid keys.ID) error {
+	key, err := s.ks.SignKey(kid)
+	if err != nil {
+		return err
+	}
+	if key == nil {
+		return keys.NewErrNotFound(kid.String())
+	}
+	logger.Infof("Pull messages...")
+	versionPath := keys.Path("versions", fmt.Sprintf("messages-%s", kid))
+	e, err := s.db.Get(ctx, versionPath)
+	if err != nil {
+		return err
+	}
+	version := ""
+	if e != nil {
+		version = string(e.Data)
+	}
+	if s.remote == nil {
+		return errors.Errorf("no remote set")
+	}
+	resp, err := s.remote.Messages(key, version)
+	if err != nil {
+		return err
+	}
+	if resp == nil {
+		logger.Infof("No messages")
+		return nil
+	}
+	logger.Infof("Received %d messages", len(resp.Messages))
+	for _, msg := range resp.Messages {
+		md := resp.MetadataFor(msg)
+		ts := 9223372036854775807 - keys.TimeToMillis(md.CreatedAt)
+		pathKey := fmt.Sprintf("messages-%s", key.ID())
+		pathVal := fmt.Sprintf("%d-%s", ts, msg.ID)
+		path := keys.Path(pathKey, pathVal)
+		if err := s.db.Set(ctx, path, msg.Data); err != nil {
+			return err
+		}
+		if err := s.saveResource(ctx, path, resp.MetadataFor(msg)); err != nil {
+			return err
+		}
+	}
+	if err := s.db.Set(ctx, versionPath, []byte(resp.Version)); err != nil {
+		return err
+	}
+	return nil
+}
---
+++ service/messages_test.go
@@ -0,0 +1,122 @@
+package service
+
+import (
+	"context"
+	"testing"
+
+	"github.com/keys-pub/keys"
+	"github.com/stretchr/testify/require"
+)
+
+func TestMessageCreateErrors(t *testing.T) {
+	env := newTestEnv(t)
+	service, closeFn := newTestService(t, env)
+	defer closeFn()
+	ctx := context.TODO()
+	testAuthSetup(t, service, alice)
+
+	randKey := keys.GenerateSignKey()
+	_, err := service.MessageCreate(ctx, &MessageCreateRequest{
+		KID:    randKey.ID().String(),
+		Sender: alice.ID().String(),
+		Text:   "test",
+	})
+	require.EqualError(t, err, "not found "+randKey.ID().String())
+}
+
+func TestMessages(t *testing.T) {
+	// SetLogger(NewLogger(DebugLevel))
+	// saltpack.SetLogger(NewLogger(DebugLevel))
+	// client.SetLogger(NewLogger(DebugLevel))
+	// server.SetContextLogger(NewContextLogger(DebugLevel))
+
+	env := newTestEnv(t)
+
+	aliceService, aliceCloseFn := newTestService(t, env)
+	defer aliceCloseFn()
+	ctx := context.TODO()
+	testAuthSetup(t, aliceService, alice)
+	testUserSetup(t, env, aliceService, alice, "alice")
+	testPush(t, aliceService, alice)
+	testRecoverKey(t, aliceService, group)
+
+	// Bob service
+	bobService, bobCloseFn := newTestService(t, env)
+	defer bobCloseFn()
+	testAuthSetup(t, bobService, bob)
+	testUserSetup(t, env, bobService, bob, "bob")
+	testRecoverKey(t, bobService, group)
+
+	// Alice lists messages
+	messagesResp, err := aliceService.Messages(ctx, &MessagesRequest{
+		KID: group.ID().String(),
+	})
+	require.NoError(t, err)
+	require.Equal(t, 0, len(messagesResp.Messages))
+
+	// Check prepare
+	_, err = aliceService.MessagePrepare(ctx, &MessagePrepareRequest{
+		KID:    group.ID().String(),
+		Sender: alice.ID().String(),
+		Text:   "prepare",
+	})
+	require.NoError(t, err)
+
+	// Alice sends 2 messages
+	_, err = aliceService.MessageCreate(ctx, &MessageCreateRequest{
+		KID:    group.ID().String(),
+		Sender: alice.ID().String(),
+		Text:   "am1",
+	})
+	require.NoError(t, err)
+
+	_, messageErrA2 := aliceService.MessageCreate(ctx, &MessageCreateRequest{
+		KID:    group.ID().String(),
+		Sender: alice.ID().String(),
+		Text:   "am2",
+	})
+	require.NoError(t, messageErrA2)
+
+	// Bob sends message
+	_, err = bobService.MessageCreate(ctx, &MessageCreateRequest{
+		KID:    group.ID().String(),
+		Sender: bob.ID().String(),
+		Text:   "bm1",
+	})
+	require.NoError(t, err)
+
+	// Alice lists messages
+	messagesResp2, err := aliceService.Messages(ctx, &MessagesRequest{
+		KID: group.ID().String(),
+	})
+	require.NoError(t, err)
+	require.Equal(t, 3, len(messagesResp2.Messages))
+
+	// cols, err := env.fi.Collections(ctx, "")
+	// // cols, err := aliceService.db.Collections(ctx, "")
+	// require.NoError(t, err)
+	// paths, err := keys.CollectionPaths(cols)
+	// require.NoError(t, err)
+	// t.Logf("cols: %+v", paths)
+
+	require.Equal(t, "am1", messagesResp2.Messages[0].Content.Text)
+	require.Equal(t, "alice", messagesResp2.Messages[0].User.Name)
+	require.Equal(t, "am2", messagesResp2.Messages[1].Content.Text)
+	require.Equal(t, "bm1", messagesResp2.Messages[2].Content.Text)
+
+	_, err = bobService.Pull(ctx, &PullRequest{KID: alice.ID().String()})
+	require.NoError(t, err)
+
+	// Bob lists messages
+	messagesResp3, err := bobService.Messages(ctx, &MessagesRequest{
+		KID: group.ID().String(),
+	})
+	require.NoError(t, err)
+	require.Equal(t, 3, len(messagesResp3.Messages))
+
+	require.Equal(t, "am1", messagesResp3.Messages[0].Content.Text)
+	require.NotNil(t, messagesResp3.Messages[0].User)
+	require.Equal(t, "alice", messagesResp3.Messages[0].User.Name)
+	require.Equal(t, "am2", messagesResp3.Messages[1].Content.Text)
+	require.Equal(t, "bm1", messagesResp3.Messages[2].Content.Text)
+}
