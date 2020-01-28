632100f0047d2bb9fb628579e25fc29ebc9ff3ab
Gabriel Handford <gabrielh@gmail.com>
2020-01-28 13:08:17 -0800

    http/api: Update keys package

--- http/api/auth_test.go
+++ http/api/auth_test.go
@@ -9,18 +9,18 @@ import (
 )
 
 func TestAuth(t *testing.T) {
-	alice := keys.NewEd25519KeyFromSeed(keys.Bytes32(bytes.Repeat([]byte{0x01}, 32)))
+	alice := keys.NewEdX25519KeyFromSeed(keys.Bytes32(bytes.Repeat([]byte{0x01}, 32)))
 
 	tm := keys.TimeFromMillis(123456789000)
 	nonce := keys.Bytes32(bytes.Repeat([]byte{0x01}, 32))
 	urs := "https://keys.pub/message?version=123456789001"
 	auth, err := newAuth("POST", urs, tm, nonce, alice)
 	require.NoError(t, err)
-	require.Equal(t, "kse132yw8ht5p8cetl2jmvknewjawt9xwzdlrk2pyxlnwjyqrdq0dawquwc7vw:sDMBYMJT7OPY1S1eP1I5jmpUSLi4QGAdg2UVooPEkHQwcie8EhfCFZeyeR7D71DkJ6vTb1bOXShmqyOqIk7l7h", auth.Header())
+	require.Equal(t, "kex132yw8ht5p8cetl2jmvknewjawt9xwzdlrk2pyxlnwjyqrdq0dawqqph077:sDMBYMJT7OPY1S1eP1I5jmpUSLi4QGAdg2UVooPEkHQwcie8EhfCFZeyeR7D71DkJ6vTb1bOXShmqyOqIk7l7h", auth.Header())
 	require.Equal(t, "https://keys.pub/message?nonce=0El6XFXwsUFD8J2vGxsaboW7rZYnQRBP5d9erwRwd29&ts=123456789000&version=123456789001", auth.URL.String())
 
 	req, err := newRequest("POST", urs, nil, tm, nonce, alice)
 	require.NoError(t, err)
 	require.Equal(t, "https://keys.pub/message?nonce=0El6XFXwsUFD8J2vGxsaboW7rZYnQRBP5d9erwRwd29&ts=123456789000&version=123456789001", req.URL.String())
-	require.Equal(t, "kse132yw8ht5p8cetl2jmvknewjawt9xwzdlrk2pyxlnwjyqrdq0dawquwc7vw:sDMBYMJT7OPY1S1eP1I5jmpUSLi4QGAdg2UVooPEkHQwcie8EhfCFZeyeR7D71DkJ6vTb1bOXShmqyOqIk7l7h", req.Header.Get("Authorization"))
+	require.Equal(t, "kex132yw8ht5p8cetl2jmvknewjawt9xwzdlrk2pyxlnwjyqrdq0dawqqph077:sDMBYMJT7OPY1S1eP1I5jmpUSLi4QGAdg2UVooPEkHQwcie8EhfCFZeyeR7D71DkJ6vTb1bOXShmqyOqIk7l7h", req.Header.Get("Authorization"))
 }
--- http/api/go.mod
+++ http/api/go.mod
@@ -3,7 +3,7 @@ module github.com/keys-pub/keysd/http/api
 go 1.12
 
 require (
-	github.com/keys-pub/keys v0.0.0-20200126194730-178e13059b7e
+	github.com/keys-pub/keys v0.0.0-20200128210451-9ae521aecfd0
 	github.com/stretchr/testify v1.4.0
 )
 
