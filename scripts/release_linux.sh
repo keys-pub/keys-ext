87205589e83eb644b8947f4bae54ba1181579b78
Gabriel Handford <gabrielh@gmail.com>
2020-02-13 09:33:45 -0800

    scripts: Seperate linux release

--- scripts/release_linux.sh
+++ scripts/release_linux.sh
@@ -4,8 +4,6 @@ set -e -u -o pipefail # Fail on error
 
 dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-skip_tag=${SKIP_TAG:-""}
-
 # Checkout to tmpdir
 tmpdir=`mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir'`
 echo "$tmpdir"
@@ -13,13 +11,9 @@ cd "$tmpdir"
 git clone https://github.com/keys-pub/keysd
 cd keysd
 
-# Tag
-if [ ! "$skip_tag" = "1" ]; then
-    $dir/tag.sh `pwd`
-fi
-
-# Other platforms
-goreleaser --rm-dist
+goreleaser --config=.goreleaser.linux.yml --rm-dist
+ver=`git describe --abbrev=0 --tags`
+$dir/aptly.sh $ver
 
 # Cleanup
 cd $dir
