026b6ca92e436ce4623926216df359415a00ed1d
Gabriel Handford <gabrielh@gmail.com>
2020-02-24 12:23:19 -0800

    service: Remove app status

--- service/status.go
+++ service/status.go
@@ -2,8 +2,6 @@ package service
 
 import (
 	"context"
-
-	"github.com/keys-pub/keys"
 )
 
 // RuntimeStatus (RPC) gets the current runtime status.
@@ -28,33 +26,4 @@ func (s *service) RuntimeStatus(ctx context.Context, req *RuntimeStatusRequest) 
 	}
 	logger.Infof("Runtime status, %s", resp.String())
 	return &resp, nil
-}
-
-func (s *service) AppStatus(ctx context.Context, req *AppStatusRequest) (*AppStatusResponse, error) {
-	ks, err := s.ks.Keys(&keys.Opts{
-		Types: []keys.KeyType{keys.EdX25519, keys.X25519},
-	})
-	if err != nil {
-		return nil, err
-	}
-	hasUser := false
-	for _, k := range ks {
-		res, err := s.users.Get(ctx, k.ID())
-		if err != nil {
-			return nil, err
-		}
-		if res != nil {
-			hasUser = true
-			break
-		}
-	}
-
-	promptKeygen := len(ks) == 0 && !s.cfg.DisablePromptKeygen()
-
-	promtUser := !hasUser && !s.cfg.DisablePromptUser()
-
-	return &AppStatusResponse{
-		PromptKeygen: promptKeygen,
-		PromptUser:   promtUser,
-	}, nil
 }
