78335625b1d402089385ec84a6ef3015eb33fdd1
Gabriel Handford <gabrielh@gmail.com>
2020-03-16 11:10:03 -0700

    Example

--- wormhole/client.go
+++ wormhole/client.go
@@ -1,11 +1,7 @@
 package wormhole
 
 import (
-	"log"
 	"net"
-	"time"
-
-	"github.com/davecgh/go-spew/spew"
 
 	"github.com/pkg/errors"
 	"gortc.io/stun"
@@ -15,6 +11,7 @@ var stunServer = "stun.l.google.com:19302"
 var udp = "udp4"
 
 type PublicAddressLn func(addr string)
+type MessageLn func(message []byte)
 
 type Client struct {
 	publicAddr      stun.XORMappedAddress
@@ -22,12 +19,14 @@ type Client struct {
 	conn            *net.UDPConn
 	quit            chan bool
 	publicAddressLn PublicAddressLn
+	messageLn       MessageLn
 }
 
 func NewClient() *Client {
 	return &Client{
 		publicAddressLn: func(addr string) {},
 		quit:            make(chan bool),
+		messageLn:       func([]byte) {},
 	}
 }
 
@@ -39,24 +38,24 @@ func (c *Client) SetPublicAddressLn(publicAddressLn PublicAddressLn) {
 	c.publicAddressLn = publicAddressLn
 }
 
+func (c *Client) SetMessageLn(messageLn MessageLn) {
+	c.messageLn = messageLn
+}
+
+// SetPeer sets peer address.
 func (c *Client) SetPeer(addr string) error {
+	logger.Infof("Set peer %s", addr)
 	a, err := net.ResolveUDPAddr(udp, addr)
 	if err != nil {
 		return err
 	}
-
-	srvAddr, err := net.ResolveUDPAddr(udp, stunServer)
-	if err != nil {
-		return errors.Wrapf(err, "failed to resolve addr")
-	}
-
-	if err := sendBindingRequest(c.conn, srvAddr); err != nil {
-		return err
-	}
-
 	c.peerAddr = a
-
 	return nil
+}
+
+// Send to peer.
+func (c *Client) Send(message []byte) error {
+	return send(message, c.conn, c.peerAddr)
 }
 
 func (c *Client) Listen() error {
@@ -67,15 +66,26 @@ func (c *Client) Listen() error {
 	c.conn = conn
 	defer c.conn.Close()
 
-	log.Printf("Listening on %s\n", c.conn.LocalAddr())
+	logger.Infof("Listening on %s", c.conn.LocalAddr())
 
 	messageChan := listen(c.conn)
-	keepAlive := time.NewTicker(time.Minute)
+	// keepAlive := time.NewTicker(time.Second)
 
+	logger.Infof("Send binding request...")
+	srvAddr, err := net.ResolveUDPAddr(udp, stunServer)
+	if err != nil {
+		return errors.Wrapf(err, "failed to resolve addr")
+	}
+	if err := sendBindingRequest(c.conn, srvAddr); err != nil {
+		return err
+	}
+
+	logger.Infof("Waiting for messages...")
 	for {
 		select {
 		case message, ok := <-messageChan:
 			if !ok {
+				logger.Infof("Listen done")
 				return nil
 			}
 			if stun.IsMessage(message) {
@@ -89,20 +99,22 @@ func (c *Client) Listen() error {
 				if getErr := xorAddr.GetFrom(m); getErr != nil {
 					return errors.Wrapf(getErr, "failed to get address from stun")
 				}
+				logger.Infof("Got STUN message: %s", xorAddr.String())
 
 				if c.publicAddr.String() != xorAddr.String() {
-					logger.Infof("Public address: %s\n", xorAddr)
+					logger.Infof("Public address: %s", xorAddr)
 					c.publicAddr = xorAddr
 					c.publicAddressLn(c.publicAddr.String())
 				}
 			} else {
-				spew.Sdump(message)
-				// Received message
+				logger.Infof("Got message (%d)", len(message))
+				c.messageLn(message)
 			}
-
-		case <-keepAlive.C:
-			// Keep alive
+		// case <-keepAlive.C:
+		// 	// Keep alive
+		// 	logger.Infof("Keep alive...")
 		case <-c.quit:
+			logger.Infof("Closing connection...")
 			c.conn.Close()
 		}
 	}
@@ -116,6 +128,7 @@ func listen(conn *net.UDPConn) <-chan []byte {
 
 			n, _, err := conn.ReadFromUDP(buf)
 			if err != nil {
+				// logger.Infof("Error reading: %v", err)
 				close(messages)
 				return
 			}
--- wormhole/client_test.go
+++ wormhole/client_test.go
@@ -1,13 +1,85 @@
 package wormhole_test
 
 import (
-	"testing"
+	"fmt"
+	"log"
+	"sync"
 
 	"github.com/keys-pub/keysd/wormhole"
 )
 
-func Test(t *testing.T) {
+func ExampleNewClient() {
 	wormhole.SetLogger(wormhole.NewLogger(wormhole.DebugLevel))
 
-	t.Fatal("testing")
+	alice := wormhole.NewClient()
+	bob := wormhole.NewClient()
+
+	peerWg := &sync.WaitGroup{}
+	peerWg.Add(2)
+
+	alice.SetPublicAddressLn(func(addr string) {
+		if err := bob.SetPeer(addr); err != nil {
+			log.Fatal(err)
+		}
+		peerWg.Done()
+	})
+	bob.SetPublicAddressLn(func(addr string) {
+		if err := alice.SetPeer(addr); err != nil {
+			log.Fatal(err)
+		}
+		peerWg.Done()
+	})
+
+	messageWg := &sync.WaitGroup{}
+	messageWg.Add(2)
+
+	alice.SetMessageLn(func(message []byte) {
+		fmt.Printf("bob: %s\n", string(message))
+		messageWg.Done()
+	})
+
+	bob.SetMessageLn(func(message []byte) {
+		fmt.Printf("alice: %s\n", string(message))
+		messageWg.Done()
+	})
+
+	// Listen
+	go func() {
+		if err := alice.Listen(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+	go func() {
+		if err := bob.Listen(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	// Wait for peer addresses
+	log.Printf("Wait for peer addresses...\n")
+	peerWg.Wait()
+	log.Printf("Got peer addresses\n")
+
+	// This message is ignored
+	if err := alice.Send([]byte("?")); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := bob.Send([]byte("hi alice")); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := alice.Send([]byte("hello bob!")); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("Waiting for messages...\n")
+	messageWg.Wait()
+	log.Printf("Got messages\n")
+
+	alice.Close()
+	bob.Close()
+	// Output:
+	// bob: hi alice
+	// alice: hello bob!
 }
