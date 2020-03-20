6e3fca134965b83e0fa32c8c1c3ec6e1ced96c67
Gabriel Handford <gabrielh@gmail.com>
2020-03-19 19:46:09 -0700

    http/server: Ephemeral

---
+++ http/server/ephem_test.go
@@ -0,0 +1,39 @@
+package server_test
+
+import (
+	"bytes"
+	"net/http"
+	"testing"
+
+	"github.com/keys-pub/keys"
+	"github.com/keys-pub/keysd/http/api"
+	"github.com/keys-pub/keysd/http/server"
+	"github.com/stretchr/testify/require"
+)
+
+func TestEphem(t *testing.T) {
+	server.SetContextLogger(server.NewContextLogger(server.DebugLevel))
+
+	clock := newClock()
+	fi := testFire(t, clock)
+	rq := keys.NewMockRequestor()
+	users := testUserStore(t, fi, rq, clock)
+	srv := newTestServer(t, clock, fi, users)
+
+	alice := keys.NewEdX25519KeyFromSeed(keys.Bytes32(bytes.Repeat([]byte{0x01}, 32)))
+	charlie := keys.NewEdX25519KeyFromSeed(keys.Bytes32(bytes.Repeat([]byte{0x03}, 32)))
+
+	// PUT /ephem/:kid/:rid/:id
+	req, err := api.NewRequest("PUT", keys.Path("ephem", alice.ID(), charlie.ID(), "offer"), bytes.NewReader([]byte("hi")), clock.Now(), alice)
+	require.NoError(t, err)
+	code, _, body := srv.Serve(req)
+	require.Equal(t, http.StatusOK, code)
+	require.Equal(t, "{}", body)
+
+	// GET /ephem/:kid/:rid/:id
+	req, err = api.NewRequest("GET", keys.Path("ephem", alice.ID(), charlie.ID(), "offer"), nil, clock.Now(), alice)
+	require.NoError(t, err)
+	code, _, body = srv.Serve(req)
+	require.Equal(t, http.StatusOK, code)
+	require.Equal(t, "hi", body)
+}
