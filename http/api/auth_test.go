59e50adf15e4faef410616dc01f81adc41c9a690
Gabriel Handford <gabrielh@gmail.com>
2020-03-20 19:29:21 -0700

    http/api: NewRequestWithContext

--- http/api/auth_test.go
+++ http/api/auth_test.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"bytes"
+	"context"
 	"testing"
 
 	"github.com/keys-pub/keys"
@@ -19,7 +20,7 @@ func TestAuth(t *testing.T) {
 	require.Equal(t, "kex132yw8ht5p8cetl2jmvknewjawt9xwzdlrk2pyxlnwjyqrdq0dawqqph077:sDMBYMJT7OPY1S1eP1I5jmpUSLi4QGAdg2UVooPEkHQwcie8EhfCFZeyeR7D71DkJ6vTb1bOXShmqyOqIk7l7h", auth.Header())
 	require.Equal(t, "https://keys.pub/message?nonce=0El6XFXwsUFD8J2vGxsaboW7rZYnQRBP5d9erwRwd29&ts=123456789000&version=123456789001", auth.URL.String())
 
-	req, err := newRequest("POST", urs, nil, tm, nonce, alice)
+	req, err := newRequest(context.TODO(), "POST", urs, nil, tm, nonce, alice)
 	require.NoError(t, err)
 	require.Equal(t, "https://keys.pub/message?nonce=0El6XFXwsUFD8J2vGxsaboW7rZYnQRBP5d9erwRwd29&ts=123456789000&version=123456789001", req.URL.String())
 	require.Equal(t, "kex132yw8ht5p8cetl2jmvknewjawt9xwzdlrk2pyxlnwjyqrdq0dawqqph077:sDMBYMJT7OPY1S1eP1I5jmpUSLi4QGAdg2UVooPEkHQwcie8EhfCFZeyeR7D71DkJ6vTb1bOXShmqyOqIk7l7h", req.Header.Get("Authorization"))
