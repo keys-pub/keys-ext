903123ffbef99466209ae87d7391bc8320112a5c
Gabriel Handford <gabrielh@gmail.com>
2020-03-24 13:57:58 -0700

    http/api: CheckAuthorization (from http/server)

--- http/api/auth.go
+++ http/api/auth.go
@@ -6,10 +6,13 @@ import (
 	"io"
 	"net/http"
 	"net/url"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/keys-pub/keys"
 	"github.com/keys-pub/keys/encoding"
+	"github.com/pkg/errors"
 )
 
 // Auth describes auth for a HTTP request.
@@ -45,6 +48,7 @@ func newAuth(method string, urs string, tm time.Time, nonce *[32]byte, key *keys
 	ur.RawQuery = q.Encode()
 
 	msg := method + "," + ur.String()
+	logger.Debugf("Signing %s", msg)
 	sb := key.SignDetached([]byte(msg))
 	sig := encoding.MustEncode(sb, encoding.Base62)
 	return &Auth{KID: key.ID(), Method: method, URL: ur, Sig: sig, Message: msg}, nil
@@ -72,4 +76,114 @@ func newRequest(ctx context.Context, method string, urs string, body io.Reader, 
 	}
 	req.Header.Set("Authorization", auth.Header())
 	return req, nil
+}
+
+// AuthResult is the authorized result.
+type AuthResult struct {
+	KID       keys.ID
+	Method    string
+	URL       *url.URL
+	Nonce     string
+	Timestamp time.Time
+}
+
+type MemCache interface {
+	// Get returns value at key.
+	Get(ctx context.Context, k string) (string, error)
+	// Put puts a value at key.
+	Set(ctx context.Context, k string, v string) error
+	// Expire key.
+	Expire(ctx context.Context, k string, dt time.Duration) error
+}
+
+// CheckAuthorization.
+func CheckAuthorization(ctx context.Context, method string, urs string, auth string, mc MemCache, now time.Time) (*AuthResult, error) {
+	fields := strings.Split(auth, ":")
+	if len(fields) != 2 {
+		return nil, errors.Errorf("too many fields")
+	}
+	skid := fields[0]
+	sig := fields[1]
+
+	kid, err := keys.ParseID(skid)
+	if err != nil {
+		return nil, err
+	}
+
+	spk, err := keys.StatementPublicKeyFromID(kid)
+	if err != nil {
+		return nil, errors.Errorf("not a valid sign public key")
+	}
+
+	sigBytes, sigerr := encoding.Decode(sig, encoding.Base62)
+	if sigerr != nil {
+		return nil, sigerr
+	}
+
+	url, err := url.Parse(urs)
+	if err != nil {
+		return nil, err
+	}
+
+	msg := method + "," + url.String()
+	logger.Infof("Checking auth for %s %s", msg, auth)
+	if err := spk.VerifyDetached(sigBytes, []byte(msg)); err != nil {
+		return nil, err
+	}
+
+	nonce := url.Query().Get("nonce")
+	if nonce == "" {
+		return nil, errors.Errorf("nonce is missing")
+	}
+	nb, err := encoding.Decode(nonce, encoding.Base62)
+	if err != nil {
+		return nil, err
+	}
+	if len(nb) != 32 {
+		return nil, errors.Errorf("nonce is invalid length")
+	}
+
+	// Check the nonce
+	nonce = fmt.Sprintf("auth-%s", nonce)
+
+	val, err := mc.Get(ctx, nonce)
+	if err != nil {
+		return nil, err
+	}
+	if val != "" {
+		return nil, errors.Errorf("nonce collision")
+	}
+	if err := mc.Set(ctx, nonce, "1"); err != nil {
+		return nil, err
+	}
+	if err := mc.Expire(ctx, nonce, time.Hour); err != nil {
+		return nil, err
+	}
+
+	// Check timestamp
+	ts := url.Query().Get("ts")
+	if ts == "" {
+		return nil, errors.Errorf("timestamp (ts) is missing")
+	}
+	i, err := strconv.Atoi(ts)
+	if err != nil {
+		return nil, err
+	}
+	tm := keys.TimeFromMillis(keys.TimeMs(i))
+	td := now.Sub(tm)
+	if td < 0 {
+		td = td * -1
+	}
+	if td > 30*time.Minute {
+		return nil, errors.Errorf("timestamp is invalid, diff %s", td)
+	}
+
+	logger.Infof("Auth OK %s", kid)
+	return &AuthResult{
+		KID:       kid,
+		Method:    method,
+		URL:       url,
+		Nonce:     nonce,
+		Timestamp: tm,
+	}, nil
 }
--- http/api/go.mod
+++ http/api/go.mod
@@ -4,6 +4,7 @@ go 1.12
 
 require (
 	github.com/keys-pub/keys v0.0.0-20200324161647-7b75199a4717
+	github.com/pkg/errors v0.9.1
 	github.com/stretchr/testify v1.4.0
 )
 
