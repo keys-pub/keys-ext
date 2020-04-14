ea2bc235b47bc1f8ee0148c30bf0f157be6cd0e3
Gabriel Handford <gabrielh@gmail.com>
2020-04-14 08:57:49 -0700

    service: Update keyring

--- service/key_test.go
+++ service/key_test.go
@@ -115,3 +115,18 @@ func TestKeyRemove(t *testing.T) {
 	_, err = service.KeyRemove(ctx, &KeyRemoveRequest{KID: randKey.ID().String()})
 	require.EqualError(t, err, fmt.Sprintf("not found %s", randKey.ID()))
 }
+
+// func TestItem(t *testing.T) {
+// 	clock := newClock()
+// 	item := keyring.NewItem("account1", []byte("password"), "passphrase", clock.Now())
+// 	b, err := msgpack.Marshal(item)
+// 	require.NoError(t, err)
+// 	expected := `([]uint8) (len=56 cap=64) {
+//  00000000  84 a2 69 64 a8 61 63 63  6f 75 6e 74 31 a3 74 79  |..id.account1.ty|
+//  00000010  70 aa 70 61 73 73 70 68  72 61 73 65 a3 64 61 74  |p.passphrase.dat|
+//  00000020  c4 08 70 61 73 73 77 6f  72 64 a3 63 74 73 d7 ff  |..password.cts..|
+//  00000030  00 3d 09 00 49 96 02 d2                           |.=..I...|
+// }
+// `
+// 	require.Equal(t, expected, spew.Sdump(b))
+// }
