ef369e13edb9431a643a8bc6829787df09cf929b
Gabriel Handford <gabrielh@gmail.com>
2020-04-26 20:56:32 -0700

    service: Update fido2

--- service/cmd_fido2.go
+++ service/cmd_fido2.go
@@ -1,13 +1,7 @@
 package service
 
 import (
-	"bytes"
-	"context"
-	"fmt"
-	"io"
-	"text/tabwriter"
-
-	"github.com/keys-pub/keysd/fido2"
+	"github.com/keys-pub/keysd/fido2/cmds"
 	"github.com/urfave/cli"
 )
 
@@ -17,39 +11,10 @@ func fido2Commands(client *Client) []cli.Command {
 			Name:  "fido2",
 			Usage: "FIDO2",
 			Subcommands: []cli.Command{
-				cli.Command{
-					Name:      "devices",
-					Usage:     "List devices",
-					Flags:     []cli.Flag{},
-					ArgsUsage: "",
-					Action: func(c *cli.Context) error {
-						req := &fido2.DetectDevicesRequest{}
-						resp, err := client.FIDO2Client().DetectDevices(context.TODO(), req)
-						if err != nil {
-							return err
-						}
-						fmtDevices(resp.Devices)
-						return nil
-					},
-				},
+				cmds.DevicesFn(client.FIDO2Client),
+				cmds.DeviceInfoFn(client.FIDO2Client),
+				cmds.MakeCredentialFn(client.FIDO2Client),
 			},
 		},
 	}
-}
-
-func fmtDevices(devices []*fido2.DeviceInfo) {
-	out := &bytes.Buffer{}
-	w := new(tabwriter.Writer)
-	w.Init(out, 0, 8, 1, ' ', 0)
-	for _, device := range devices {
-		fmtDevice(w, device)
-	}
-	if err := w.Flush(); err != nil {
-		panic(err)
-	}
-	fmt.Print(out.String())
-}
-
-func fmtDevice(w io.Writer, device *fido2.DeviceInfo) {
-	fmt.Fprintf(w, "%s\t%s\t%s\t%d\t%d\n", device.Product, device.Manufacturer, device.Path, device.ProductID, device.VendorID)
 }
--- service/go.mod
+++ service/go.mod
@@ -8,7 +8,7 @@ require (
 	github.com/grpc-ecosystem/go-grpc-middleware v1.2.0
 	github.com/keys-pub/keys v0.0.0-20200423195135-6b5d508024d0
 	github.com/keys-pub/keysd/db v0.0.0-20200413003215-f85e85366c95
-	github.com/keys-pub/keysd/fido2 v0.0.0-20200425222705-334f0c7b66f7
+	github.com/keys-pub/keysd/fido2 v0.0.0-20200427035443-641310d59016
 	github.com/keys-pub/keysd/http/api v0.0.0-20200415010142-cfcd41d36dd1
 	github.com/keys-pub/keysd/http/client v0.0.0-20200414170516-1e0e72775735
 	github.com/keys-pub/keysd/http/server v0.0.0-20200419021946-2d1a17136bd0
@@ -18,9 +18,9 @@ require (
 	github.com/mitchellh/go-ps v1.0.0
 	github.com/onsi/gomega v1.7.0 // indirect
 	github.com/pkg/errors v0.9.1
-	github.com/sirupsen/logrus v1.4.2
+	github.com/sirupsen/logrus v1.5.0
 	github.com/stretchr/testify v1.5.1
-	github.com/urfave/cli v1.22.3
+	github.com/urfave/cli v1.22.4
 	golang.org/x/crypto v0.0.0-20200406173513-056763e48d71
 	google.golang.org/grpc v1.29.1
 )
@@ -30,6 +30,7 @@ require (
 // replace github.com/keys-pub/keysd/db => ../db
 
 // replace github.com/keys-pub/keysd/fido2 => ../fido2
+// replace github.com/keys-pub/go-libfido2 => ../../go-libfido2
 
 // replace github.com/keys-pub/keysd/http/api => ../http/api
 
