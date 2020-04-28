abf127797c498d23dc3b01478007d4db0e8a042d
Gabriel Handford <gabrielh@gmail.com>
2020-04-27 19:24:04 -0700

    Revert "service: Move fido2 (temporarily)"
    This reverts commit f077689e0fd6420c9a599dc16a4607f41a6474c2.

--- service/cmd_fido2.go
+++ service/cmd_fido2.go
@@ -1,15 +1,20 @@
 package service
 
-// func fido2Commands(client *Client) []cli.Command {
-// 	return []cli.Command{
-// 		cli.Command{
-// 			Name:  "fido2",
-// 			Usage: "FIDO2",
-// 			Subcommands: []cli.Command{
-// 				cmds.DevicesFn(client.FIDO2Client),
-// 				cmds.DeviceInfoFn(client.FIDO2Client),
-// 				cmds.MakeCredentialFn(client.FIDO2Client),
-// 			},
-// 		},
-// 	}
-// }
+import (
+	"github.com/keys-pub/keysd/fido2/cmds"
+	"github.com/urfave/cli"
+)
+
+func fido2Commands(client *Client) []cli.Command {
+	return []cli.Command{
+		cli.Command{
+			Name:  "fido2",
+			Usage: "FIDO2",
+			Subcommands: []cli.Command{
+				cmds.DevicesFn(client.FIDO2Client),
+				cmds.DeviceInfoFn(client.FIDO2Client),
+				cmds.MakeCredentialFn(client.FIDO2Client),
+			},
+		},
+	}
+}
--- service/go.mod
+++ service/go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/grpc-ecosystem/go-grpc-middleware v1.2.0
 	github.com/keys-pub/keys v0.0.0-20200423195135-6b5d508024d0
 	github.com/keys-pub/keysd/db v0.0.0-20200413003215-f85e85366c95
+	github.com/keys-pub/keysd/fido2 v0.0.0-20200427220403-902714bb307e
 	github.com/keys-pub/keysd/http/api v0.0.0-20200415010142-cfcd41d36dd1
 	github.com/keys-pub/keysd/http/client v0.0.0-20200414170516-1e0e72775735
 	github.com/keys-pub/keysd/http/server v0.0.0-20200419021946-2d1a17136bd0
