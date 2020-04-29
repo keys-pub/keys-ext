672c75982d02c5792de19c5013d2d779bdf9d442
Gabriel Handford <gabrielh@gmail.com>
2020-04-28 20:50:47 -0700

    service: fido2Commands

--- service/cmd_fido2.go
+++ service/cmd_fido2.go
@@ -1,19 +1,60 @@
 package service
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/keys-pub/keysd/fido2"
 	"github.com/urfave/cli"
 )
 
 func fido2Commands(client *Client) []cli.Command {
 	return []cli.Command{
-		// cli.Command{
-		// 	Name:  "fido2",
-		// 	Usage: "FIDO2",
-		// 	Subcommands: []cli.Command{
-		// 		cmds.DevicesFn(client.FIDO2Client),
-		// 		cmds.DeviceInfoFn(client.FIDO2Client),
-		// 		cmds.MakeCredentialFn(client.FIDO2Client),
-		// 	},
-		// },
+		cli.Command{
+			Name:  "fido2",
+			Usage: "FIDO2",
+			Subcommands: []cli.Command{
+				cli.Command{
+					Name:  "devices",
+					Usage: "Show devices",
+					Flags: []cli.Flag{},
+					Action: func(c *cli.Context) error {
+						req := &fido2.DevicesRequest{}
+						resp, err := client.FIDO2Client().Devices(context.TODO(), req)
+						if err != nil {
+							return err
+						}
+						printResponse(resp)
+						return nil
+					},
+				},
+				cli.Command{
+					Name:      "device-info",
+					Usage:     "Device info",
+					Flags:     []cli.Flag{},
+					ArgsUsage: "<device path>",
+					Action: func(c *cli.Context) error {
+						req := fido2.DeviceInfoRequest{
+							Device: c.Args().First(),
+						}
+						resp, err := client.FIDO2Client().DeviceInfo(context.TODO(), &req)
+						if err != nil {
+							return err
+						}
+						printResponse(resp)
+						return nil
+					},
+				},
+			},
+		},
 	}
+}
+
+func printResponse(i interface{}) {
+	b, err := json.MarshalIndent(i, "", "  ")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(b))
 }
