fa25a4ec201c3ceaafbc7add19751e72df2ce505
Gabriel Handford <gabrielh@gmail.com>
2020-05-05 14:12:25 -0700

    scripts: Don't tag automatically

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,7 +4,7 @@ set -e -u -o pipefail # Fail on error
 
 dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-skip_tag=${SKIP_TAG:-""}
+#skip_tag=${SKIP_TAG:-""}
 
 # Checkout to tmpdir
 tmpdir=`mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir'`
@@ -14,12 +14,12 @@ git clone https://github.com/keys-pub/keysd
 cd keysd
 
 # Tag
-if [ ! "$skip_tag" = "1" ]; then
-    $dir/tag.sh `pwd`
-fi
+# if [ ! "$skip_tag" = "1" ]; then
+#     $dir/tag.sh `pwd`
+# fi
 
 cd service
-goreleaser --rm-dist
+goreleaser
 
 # Cleanup
 cd $dir
