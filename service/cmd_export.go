1cabfde08c6c47d1aba811d88e9d0d25974bed25
Gabriel Handford <gabrielh@gmail.com>
2020-05-17 11:59:46 -0700

    wip

--- service/cmd_export.go
+++ service/cmd_export.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/pkg/errors"
 	"github.com/urfave/cli"
 )
 
@@ -14,7 +15,10 @@ func exportCommands(client *Client) []cli.Command {
 			Usage: "Export a key",
 			Flags: []cli.Flag{
 				cli.StringFlag{Name: "kid, k", Usage: "kid"},
+				cli.StringFlag{Name: "type, t", Value: "default", Usage: "default, saltpack, ssh"},
+				cli.BoolFlag{Name: "public", Usage: "export public key part only"},
 				cli.StringFlag{Name: "password, p", Usage: "password"},
+				cli.BoolFlag{Name: "no-password", Usage: "export without password"},
 			},
 			Action: func(c *cli.Context) error {
 				kid, err := argString(c, "kid", false)
@@ -22,19 +26,31 @@ func exportCommands(client *Client) []cli.Command {
 					return err
 				}
 
+				typ, err := exportTypeFromString(c.String("type"))
+				if err != nil {
+					return err
+				}
+
 				password := c.String("password")
-				if len(password) == 0 {
-					p, err := readPassword("Enter the password:")
-					if err != nil {
-						return err
+				public := c.Bool("public")
+				noPassword := c.Bool("no-password")
+
+				if !public && !noPassword {
+					if len(password) == 0 {
+						p, err := readPassword("Enter the password:")
+						if err != nil {
+							return err
+						}
+						password = p
 					}
-					password = p
 				}
 
 				req := &KeyExportRequest{
-					KID:      kid,
-					Password: password,
-					Type:     SaltpackExportType,
+					KID:        kid,
+					Password:   password,
+					Type:       typ,
+					Public:     public,
+					NoPassword: noPassword,
 				}
 				resp, err := client.KeysClient().KeyExport(context.TODO(), req)
 				if err != nil {
@@ -44,5 +60,18 @@ func exportCommands(client *Client) []cli.Command {
 				return nil
 			},
 		},
+	}
+}
+
+func exportTypeFromString(s string) (ExportType, error) {
+	switch s {
+	case "", "default":
+		return DefaultExport, nil
+	case "saltpack":
+		return SaltpackExport, nil
+	case "ssh":
+		return SSHExport, nil
+	default:
+		return DefaultExport, errors.Errorf("invalid type: %s", s)
 	}
 }
