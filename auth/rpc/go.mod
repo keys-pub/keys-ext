50934be771411d2d97e13c7499e956b93f1d3b97
Gabriel Handford <gabrielh@gmail.com>
2020-05-22 13:34:34 -0700

    auth: RPC

---
+++ auth/rpc/go.mod
@@ -0,0 +1,12 @@
+module github.com/keys-pub/keysd/auth/rpc
+
+go 1.14
+
+require (
+	github.com/google/uuid v1.1.1
+	github.com/keys-pub/go-libfido2 v1.4.1-0.20200522191908-864232a8fc63
+	github.com/keys-pub/keysd/auth/fido2 v0.0.0-20200522202857-738099e14111
+	github.com/pkg/errors v0.9.1
+	github.com/stretchr/testify v1.5.1
+	google.golang.org/grpc v1.29.1
+)
---
+++ auth/rpc/server_test.go
@@ -0,0 +1,99 @@
+package rpc_test
+
+import (
+	context "context"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/keys-pub/keysd/auth/fido2"
+	"github.com/keys-pub/keysd/auth/rpc"
+	"github.com/stretchr/testify/require"
+)
+
+func TestInfo(t *testing.T) {
+	ctx := context.TODO()
+	server := rpc.NewAuthenticatorsServer()
+
+	resp, err := server.Devices(ctx, &fido2.DevicesRequest{})
+	require.NoError(t, err)
+
+	for _, device := range resp.Devices {
+		require.NotEmpty(t, device.Path)
+
+		infoResp, err := server.DeviceInfo(ctx, &fido2.DeviceInfoRequest{
+			Device: device.Path,
+		})
+		require.NoError(t, err)
+		t.Logf("Info: %+v", infoResp.Info)
+		require.NotEmpty(t, infoResp.Info.AAGUID)
+	}
+}
+
+func ExampleAuthenticatorsServer_SetPIN() {
+	if os.Getenv("FIDO2_EXAMPLES") != "1" {
+		return
+	}
+
+	ctx := context.TODO()
+	server := rpc.NewAuthenticatorsServer()
+
+	resp, err := server.Devices(ctx, &fido2.DevicesRequest{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	if len(resp.Devices) < 1 {
+		log.Fatal("No devices")
+	}
+	if len(resp.Devices) != 1 {
+		log.Fatal("Too many devices")
+	}
+	path := resp.Devices[0].Path
+
+	_, err = server.SetPIN(ctx, &fido2.SetPINRequest{
+		Device: path,
+		PIN:    "123456",
+		OldPIN: "sdflfwdsasdfsadf",
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Output:
+	//
+}
+
+func ExampleAuthenticatorsServer_Credentials() {
+	if os.Getenv("FIDO2_EXAMPLES") != "1" {
+		return
+	}
+
+	ctx := context.TODO()
+	server := rpc.NewAuthenticatorsServer()
+
+	dresp, err := server.Devices(ctx, &fido2.DevicesRequest{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	if len(dresp.Devices) < 1 {
+		log.Fatal("No devices")
+	}
+	if len(dresp.Devices) != 1 {
+		log.Fatal("Too many devices")
+	}
+	path := dresp.Devices[0].Path
+
+	cresp, err := server.Credentials(ctx, &fido2.CredentialsRequest{
+		Device: path,
+		PIN:    "12345",
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, cred := range cresp.Credentials {
+		log.Printf("%+v\n", cred)
+	}
+
+	// Output:
+	//
+}
