55b951069853fad549f357e973ff028ba403fd3a
Gabriel Handford <gabrielh@gmail.com>
2020-05-22 13:39:15 -0700

    auth/fido: AuthServer

--- auth/fido2/plugin.go
+++ auth/fido2/plugin.go
@@ -7,19 +7,19 @@ import (
 )
 
 // OpenPlugin returns AuthsServer from shared library.
-func OpenPlugin(path string) (AuthsServer, error) {
+func OpenPlugin(path string) (AuthServer, error) {
 	plug, err := plugin.Open(path)
 	if err != nil {
 		return nil, err
 	}
-	symLibrary, err := plug.Lookup("AuthsServer")
+	symLibrary, err := plug.Lookup("AuthServer")
 	if err != nil {
 		return nil, err
 	}
 
-	lib, ok := symLibrary.(AuthsServer)
+	lib, ok := symLibrary.(AuthServer)
 	if !ok {
-		return nil, errors.Errorf("not AuthsServer library")
+		return nil, errors.Errorf("not AuthServer library")
 	}
 	return lib, nil
 }
