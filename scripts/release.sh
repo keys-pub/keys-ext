bfbba2770e53842b8424e62db9d21b0f0cffb0a1
Gabriel Handford <gabrielh@gmail.com>
2020-05-23 13:38:45 -0700

    auth/rpc/plugin: goreleaser

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,8 +4,6 @@ set -e -u -o pipefail # Fail on error
 
 dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-#skip_tag=${SKIP_TAG:-""}
-
 # Checkout to tmpdir
 tmpdir=`mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir'`
 echo "$tmpdir"
@@ -13,14 +11,13 @@ cd "$tmpdir"
 git clone https://github.com/keys-pub/keysd
 cd keysd
 
-# Tag
-# if [ ! "$skip_tag" = "1" ]; then
-#     $dir/tag.sh `pwd`
-# fi
-
+# keys, keysd
 cd service
 goreleaser
-goreleaser --config=.goreleaser.fido2.yml --rm-dist
+
+# fido2.so
+cd ../auth/rpc/plugin
+goreleaser
 
 # Cleanup
 cd $dir
