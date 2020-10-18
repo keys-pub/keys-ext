3e9c4f9b301d9f727ff1f5f36a3418329bed55ff
Gabriel Handford <gabrielh@gmail.com>
2020-10-17 17:06:34 -0700

    auth/rpc/plugin: Fix export (FIDO2Server)

--- auth/rpc/plugin/plugin.go
+++ auth/rpc/plugin/plugin.go
@@ -4,8 +4,8 @@ import (
 	"github.com/keys-pub/keys-ext/auth/rpc"
 )
 
-// AuthServer exported for plugin.
-var AuthServer = rpc.Server{} // nolint
+// FIDO2Server exported for plugin.
+var FIDO2Server = rpc.Server{} // nolint
 
 // This is a plugin, so main isn't necessary, but we need it for goreleaser.
 func main() {}
