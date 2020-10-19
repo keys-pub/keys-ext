5df96ff1cb67ea644ffbd73976204356be01e4ed
Gabriel Handford <gabrielh@gmail.com>
2020-10-19 15:37:43 -0700

    scripts: Remove tsclient proto

--- service/proto.sh
+++ service/proto.sh
@@ -4,12 +4,5 @@ set -e -u -o pipefail # Fail on error
 
 dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd "$dir"
-keysapp="$dir/../../app"
 
 "$dir/../scripts/proto3.sh" "$dir"
-
-cp *.proto "$keysapp/src/main/rpc/"
-cp *.d.ts "$keysapp/src/main/rpc/"
-cp *.d.ts "$keysapp/src/renderer/rpc/"
-rm *.d.ts
-mv *.ts "$keysapp/src/renderer/rpc/"
