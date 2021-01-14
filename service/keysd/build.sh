547c767c41b0d4e6ebf62a57d2da5a75a641efa3
Gabriel Handford <gabrielh@gmail.com>
2021-01-14 13:02:30 -0800

    service: Update goreleaser

---
+++ service/keysd/build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e -u -o pipefail # Fail on error
+
+dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd "$dir"
+
+scripts="$dir/../../scripts"
+
+ts=`date +%s`
+date=`date -u +"%Y-%m-%dT%H:%M:%SZ"`
+ver="0.0.0-dev-$ts"
+
+DEBUG=1 VERSION=$ver DATE=$date "$scripts/gobuild.sh" keysd "$dir"
+DEBUG=1 VERSION=$ver DATE=$date "$scripts/gobuild.sh" keys  "$dir/../keys"
+
