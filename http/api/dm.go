d34ec0c002a0745f95f6fabe706e4193c3e1469c
Gabriel Handford <gabrielh@gmail.com>
2021-03-29 11:07:39 -0700

    http/api: -Message

--- http/api/dm.go
+++
@@ -1,9 +0,0 @@
-package api
-
-import "github.com/keys-pub/keys"
-
-// DirectToken ...
-type DirectToken struct {
-	User  keys.ID `json:"user" msgpack:"user"`
-	Token string  `json:"token" msgpack:"token"`
-}
--- http/api/event.go
+++ http/api/event.go
@@ -1,7 +1,6 @@
 package api
 
 import (
-	"github.com/keys-pub/keys"
 	"github.com/keys-pub/keys/dstore/events"
 )
 
@@ -21,18 +20,4 @@ type Events struct {
 	Events    []*events.Event `json:"events"`
 	Index     int64           `json:"idx"`
 	Truncated bool            `json:"truncated,omitempty"`
-}
-
-// Decrypt events into messages.
-func (e Events) Decrypt(key *keys.EdX25519Key) ([]*Message, error) {
-	msgs := make([]*Message, 0, len(e.Events))
-	for _, event := range e.Events {
-		msg, err := DecryptMessageFromEvent(event, key)
-		if err != nil {
-			// TODO: Skip invalid messages
-			return nil, err
-		}
-		msgs = append(msgs, msg)
-	}
-	return msgs, nil
 }
